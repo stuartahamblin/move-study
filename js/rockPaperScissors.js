e8c0b9a3e63b63f9c946f82ebdc97224d9104675
stuartahamblin <stuart.a.hamblin@gmail.com>
2019-04-03 20:31:53 -0500

    finish rock paper scissors

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -9,42 +9,74 @@ opponent = {
 const attack = (playerThrow) => {
     const possibleThrows = ["rock", "paper", "scissors"];
     const opponentThrow = possibleThrows[Math.floor(Math.random()*3)];
-    document.getElementById('player-choice').innerText = playerThrow;
+    const roundMessage = document.getElementById('round-message');
+    const playerChoice = document.getElementById('player-choice');
+    const opponentChoice = document.getElementById('opponent-choice');
+    const playerThrowsLeft = document.getElementById('player-throws-left');
+    const opponentThrowsLeft = document.getElementById('opponent-throws-left');
+    $('.btn').attr('disabled', true);
+    playerChoice.innerText = playerThrow.toUpperCase();
+    opponentChoice.innerText = opponentThrow.toUpperCase();
     setTimeout(() => {
-        document.getElementById('opponent-choice').innerText = opponentThrow;
+        if (playerThrow === "rock" ){
+            if (opponentThrow === "rock"){
+                // return
+                roundMessage.innerText = "DRAW"
+            } else if (opponentThrow === "paper"){
+                player.throwsLeft -= 1;
+                roundMessage.innerText = "PAPER WINS"
+            } else if (opponentThrow === "scissors"){
+                opponent.throwsLeft -= 1;
+                roundMessage.innerText = "ROCK WINS"
+            }
+        } else if (playerThrow === "paper"){
+            if (opponentThrow === "rock"){
+                opponent.throwsLeft -= 1;
+                roundMessage.innerText = "PAPER WINS"
+            } else if (opponentThrow === "paper"){
+                // return
+                roundMessage.innerText = "DRAW"
+            } else if (opponentThrow === "scissors"){
+                player.throwsLeft -= 1;
+                roundMessage.innerText = "SCISSORS WINS"
+            }
+        } else if (playerThrow === "scissors"){
+            if (opponentThrow === "rock"){
+                player.throwsLeft -= 1;
+                roundMessage.innerText = "ROCK WINS"
+            } else if (opponentThrow === "paper"){
+                opponent.throwsLeft -= 1;
+                roundMessage.innerText = "SCISSORS WINS"
+            } else if (opponentThrow === "scissors"){
+                // return
+                roundMessage.innerText = "DRAW"
+            }
+        }
+        setTimeout(() => {
+            if (player.throwsLeft <= 0){
+                roundMessage.innerText = "OPPONENT WINS";
+                playerChoice.innerText = " ";
+                opponentChoice.innerText = " ";
+                playerThrowsLeft.innerText = player.throwsLeft;
+                opponentThrowsLeft.innerText = opponent.throwsLeft;
+                return
+            } else if (opponent.throwsLeft <= 0){
+                roundMessage.innerText = "PLAYER WINS";
+                playerChoice.innerText = " ";
+                opponentChoice.innerText = " ";
+                playerThrowsLeft.innerText = player.throwsLeft;
+                opponentThrowsLeft.innerText = opponent.throwsLeft;
+                return
+            }
+            playerThrowsLeft.innerText = player.throwsLeft;
+            opponentThrowsLeft.innerText = opponent.throwsLeft;
+            playerChoice.innerText = " ";
+            opponentChoice.innerText = " ";
+            roundMessage.innerText = " ";
+            $('.btn').attr('disabled', false);
+        }, 1000);
     }, 1000);
-    setTimeout(() => {
 
-    }, 1000);
-    setTimeout(() => {
-        document.getElementById('player-throws-left').innerText = player.throwsLeft;
-        document.getElementById('opponent-throws-left').innerText = opponent.throwsLeft
-    }, 1000);
-    if (playerThrow === "rock" ){
-        if (opponentThrow === "rock"){
-            // return
-        } else if (opponentThrow === "paper"){
-            player.throwsLeft -= 1;
-        } else if (opponentThrow === "scissors"){
-            opponent.throwsLeft -= 1;
-        }
-    } else if (playerThrow === "paper"){
-        if (opponentThrow === "rock"){
-            opponent.throwsLeft -= 1;
-        } else if (opponentThrow === "paper"){
-            // return
-        } else if (opponentThrow === "scissors"){
-            player.throwsLeft -= 1;
-        }
-    } else if (playerThrow === "scissors"){
-        if (opponentThrow === "rock"){
-            player.throwsLeft -= 1;
-        } else if (opponentThrow === "paper"){
-            opponent.throwsLeft -= 1;
-        } else if (opponentThrow === "scissors"){
-            // return
-        }
-    }
     console.log("Player: " + playerThrow + " , Opponent: " + opponentThrow);
     console.log("Player throws left: " + player.throwsLeft + " , Opponent throws left: " + opponent.throwsLeft)
 };
